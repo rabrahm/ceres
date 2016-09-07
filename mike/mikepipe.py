4e420775a9bd8db6f8046230dd6dc3ca1dbecbac
Rafael Brahm <rbrahm@gmail.com>
2016-09-07 03:33:19 -0300

    Update mikepipe.py

--- mike/mikepipe.py
+++ mike/mikepipe.py
@@ -7,7 +7,7 @@ sys.path.append(base+"utils/Continuum")
 sys.path.append(base+"utils/Correlation")
 sys.path.append(base+"utils/OptExtract")
 
-baryc_dir= base+'utils/SSEphemx/'
+baryc_dir= base+'utils/SSEphem/'
 sys.path.append(baryc_dir)
 ephemeris='DEc403'
 
