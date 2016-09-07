766bc5f342197ec1b0797d7dbab3fab6ed5e97d0
Rafael Brahm <rbrahm@gmail.com>
2016-09-07 03:09:02 -0300

    Update setup.py

--- utils/Continuum/setup.py
+++ utils/Continuum/setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup, Extension
+import numpy
 
-module =Extension('FunNorm', sources = ['FunNorm.c'],libraries=['gsl','gslcblas','m'])
+module =Extension('FunNorm', sources = ['FunNorm.c'],libraries=['gsl','gslcblas','m'],include_dirs=[numpy.get_include()+'/numpy'])
 setup(name = 'Funciones creadas por Nestor: Extensiones de C/Python', version = '1.0', ext_modules = [module])
