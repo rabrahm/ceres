edc768ae552a67a5218be6a36691b6522b61a7a4
Rafael Brahm <rbrahm@gmail.com>
2016-09-07 03:32:38 -0300

    Update harpspipe.py

--- harps/harpspipe.py
+++ harps/harpspipe.py
@@ -7,7 +7,7 @@ sys.path.append(base+"utils/Correlation")
 sys.path.append(base+"utils/OptExtract")
 sys.path.append(base+"utils/GLOBALutils")
 
-baryc_dir= base+'utils/SSEphemx/'
+baryc_dir= base+'utils/SSEphem/'
 sys.path.append(baryc_dir)
 ephemeris='DEc403'
 
